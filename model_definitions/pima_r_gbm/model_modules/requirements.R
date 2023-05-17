dc529c6a9231cca2edbe5e3d712b57d64d19a589
Christian-TD <christianaxel.schmidt@teradata.com>
2023-05-17 22:39:10 +0000

    Added R PIMA GBM demo model 🎢

---
+++ model_definitions/pima_r_gbm/model_modules/requirements.R
@@ -0,0 +1,4 @@
+message('Installing packages')
+if(!require('gbm')){install.packages('gbm')}
+if(!require('devtools')){install.packages('devtools')}
+if(!require('caret')){install.packages('caret')}
