7bc259322545bd9a95f97d911f588173512e858b
Christian Schmidt <52788365+Christian-TD@users.noreply.github.com>
2023-12-22 18:49:09 +0100

    Update requirements.R

--- model_definitions/r-diabetes/model_modules/requirements.R
+++ model_definitions/r-diabetes/model_modules/requirements.R
@@ -1,7 +1,7 @@
 message('Installing packages')
-if(!require('gbm')){install.packages('gbm')}
-if(!require('devtools')){install.packages('devtools')}
-if(!require('caret')){install.packages('caret')}
+if(!require('gbm')){install.packages('gbm',repos = "http://cran.us.r-project.org")}
+if(!require('devtools')){install.packages('devtools',repos = "http://cran.us.r-project.org")}
+if(!require('caret')){install.packages('caret',repos = "http://cran.us.r-project.org")}
 
 #library("devtools")
 #install_git("git://github.com/jpmml/r2pmml.git")
